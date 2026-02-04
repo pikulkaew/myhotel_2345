458b43465b6c0ba0b050e09b35977883af87ec25
1234 Pikulkaew Tangtisanon <kaew.ite@gmail.com>
2026-02-04 11:28:13 +0700

    final

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// Connect to server and select database.
+define('servername', 'localhost');
+define('username', 'root');
+define('password', '');
+define('dbname', 'myhotel_2345');
+
+$objCon = mysqli_connect(servername, username, password,dbname);
+
+
+
+
+?>
+
